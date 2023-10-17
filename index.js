3e06cb3f4a64e01834f107f17f9e4c74c595a455
mendelcohen <mmhakohen@gmail.com>
2023-10-16 21:55:16 -0400

    fixes handling api calls not included in the backend

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ app.use(
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "./public")));
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+// Handle GET requests to /api route
+app.get("/index", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 const auth = require("./auth");
 
